40bd6a11174df6b4af9172c7eb8bc11d99e58f76
David Meister <thedavidmeister@gmail.com>
2020-09-14 19:19:08 +0400

    working svelte example

--- examples/svelte/src/App.svelte
+++ examples/svelte/src/App.svelte
@@ -1,22 +1,22 @@
 <script>
+export let old;
 export let offset;
 
 import 'javelin-timesync';
 
 javelin_timesync.core.offset_cb(
- (context, key, ref, o, n) => {
-  console.log('old', o)
-  console.log('new', n)
+ (key, ref, o, n) => {
+  old = o
+  offset = n
  },
 	"https://rest.ably.io/time?v=1.0",
 	{'parse': (v) => { return v[0] }, 'interval': 2000, 'data-points': 10}
 )
-
 </script>
 
 <main>
 	<h1>Hello</h1>
-	<p>Open the console to see the offset_cb.</p>
+	<p>Old: {old} Current: {offset}</p>
 </main>
 
 <style>
